3f63bfeaf4693136f91527d970eefa8728729f2f
Tsuyoshi <tsuyoshi@MacBookPro.local>
2010-06-28 23:16:26 +0900

    add task D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,11 @@ class StoreController < ApplicationController
   def add_to_cart
     product = Product.find(params[:id])
     @cart = find_cart
-    @cart.add_product(product)
+    @current_item = @cart.add_product(product)
+    respond_to do |format|
+      format.js if request.xhr?
+      format.html {redirect_to_index}
+    end
   rescue ActiveRecord::RecordNotFound
     logger.error("invalid access to product #{params[:id]} .")
     redirect_to_index("invalid product")
@@ -11,16 +15,17 @@ class StoreController < ApplicationController
   
   def empty_cart
     session[:cart] = nil
-    redirect_to_index("Your cart has no items.")
+    redirect_to_index
   end
   
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
 
 private
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg=nil)
+    flash[:notice] = msg if msg
     redirect_to :action => 'index'
   end
   
